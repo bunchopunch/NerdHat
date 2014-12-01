5c371c52c8e4120a0ceca31e19d5390a55ad8426
Ami G <bunchopunch@mustacheforpresident.com>
2014-12-01 12:38:17 -0700

    Cleaning up require config

--- client/app/require-config.js
+++ client/app/require-config.js
@@ -2,46 +2,45 @@
  
 require.config({
  
-   baseUrl: 'scripts',
+  baseUrl: 'scripts',
  
-   paths: {
-      'jquery': '../../bower_components/jquery/dist/jquery',
-      'modernizr': '../../bower_components/modernizr/modernizr',
-      'backbone': '../../bower_components/backbone/backbone',
-      'underscore': '../../bower_components/underscore/underscore',
-      'marionette': '../../bower_components/marionette/lib/backbone.marionette',
-      'foundation': '../../bower_components/foundation/js/foundation',
-      'text': '../../bower_components/text/text',
-      'vendor': '../../vendor_components'
-   },
- 
-   shim: {
-     underscore: {
-       exports: '_'
-     },
-     backbone: {
-       deps: [
-         'underscore',
-         'jquery'
-       ],
-       exports: 'Backbone'
-     },
-     marionette: {
-       deps: ['backbone'],
-       exports: 'Backbone.Marionette'
-     },
-     foundation: {
-       deps: [
-         'jquery',
-         'modernizr'
-       ],
-       exports: 'Foundation'
-     },
-     modernizr: {
-       exports: 'Modernizr'
-     },
-   },
- 
+  paths: {
+    'jquery': '../../bower_components/jquery/dist/jquery',
+    'modernizr': '../../bower_components/modernizr/modernizr',
+    'backbone': '../../bower_components/backbone/backbone',
+    'underscore': '../../bower_components/underscore/underscore',
+    'marionette': '../../bower_components/marionette/lib/backbone.marionette',
+    'foundation': '../../bower_components/foundation/js/foundation',
+    'text': '../../bower_components/text/text',
+    'vendor': '../../vendor_components'
+  }, 
+  
+  shim: {
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: [
+        'underscore',
+        'jquery'
+      ],
+      exports: 'Backbone'
+    },
+    marionette: {
+      deps: ['backbone'],
+      exports: 'Backbone.Marionette'
+    },
+    foundation: {
+      deps: [
+        'jquery',
+        'modernizr'
+      ],
+      exports: 'Foundation'
+    },
+    modernizr: {
+      exports: 'Modernizr'
+    },
+  },
 });
  
 require(['main'], function() {});
