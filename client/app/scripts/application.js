1b93c17929dd7c4120761c74e4ae8bfe8fd2b981
Ami G <bunchopunch@mustacheforpresident.com>
2014-12-16 17:54:20 -0700

    Moved templates to external files

--- client/app/scripts/application.js
+++ client/app/scripts/application.js
@@ -12,7 +12,7 @@ define([
   console.log('Application was loaded');
 
   var initialize = function() {
-  	// Start Foundation
+    // Start Foundation
     $(document).foundation();
     // Set up a Marionette Application
     var App = new Backbone.Marionette.Application();
@@ -21,7 +21,7 @@ define([
     App.root = 'http://localhost:9090/api/';
 
     // We will eventually require templates here, but with a dynamic culture/language code.
-    // Also.... need to not async load templates.
+    // If we choose to go down that road.
 
     // THE APP. STUFF THAT WILL EVENTUALLY BE BROKEN OUT TO MODULES =============================
     App.addRegions({
@@ -61,31 +61,31 @@ define([
 
     // VIEWS (ALSO TO BE BROKEN OUT) ============================================================
 
-//    As long as we're dropping the item view for now...
-//    var HatView = Backbone.Marionette.ItemView.extend();
+    // As long as we're dropping the item view for now...
+    // var HatView = Backbone.Marionette.ItemView.extend();
 
     var HatCompositeView = Backbone.Marionette.CompositeView.extend({
       className: 'panel clearfix hat',
       childViewContainer: '#collectionOutput',
-      template: '#collection_item_template', // How Marionette would normally handle templates. Ew.
+      template: _.template(HatsViewSingleTemp)
 
-//      Doesn't seem that this bit is needed:
-//      ItemView: HatView
+      // Doesn't seem that this bit is needed:
+      // ItemView: HatView
 
-//      May want to add this once we start updating the collection:
-//        modelEvents: {
-//          'change': 'render'
-//        },
+      // May want to add this once we start updating the collection:
+      // modelEvents: {
+      //   'change': 'render'
+      // },
     });
 
     var hatCollectionView = new HatCompositeView({
       id: 'primaryPanel',           // If we don't add an ID and class here it will
       className: 'hatCollection',   // get the ones from the constructor.
       collection: indexHats,
-      template: '#collection_template'
+      template: _.template(HatsViewTemp)
     });
 
-    // SET UP START LISTNER =======================================================================
+    // SET UP START LISTNER ======================================================================
     App.on('start', function() {
       App.hatController = new App.Controller();
       App.router = new App.Router({
