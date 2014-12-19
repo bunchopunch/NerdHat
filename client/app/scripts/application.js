3b6b27068682e3ec64f9b8bee6e024eec913e1e0
Ami G <bunchopunch@mustacheforpresident.com>
2014-12-19 15:11:35 -0700

    Cleaning minor bits

--- client/app/scripts/application.js
+++ client/app/scripts/application.js
@@ -24,11 +24,12 @@ define([
     primaryViewport: '#applicationHost'
   });
 
+  // It would be neat to abstract routes so they aren't hard coded.
   App.Router = Marionette.AppRouter.extend({
     appRoutes: {
-      '': 'hatCollection',        // http://localhost:9000/# - Not yet implemented.
-      'hats': 'hatCollection',    // http://localhost:9000/#/hats
-      'hats/:id': 'hatCollection' // http://localhost:9000/#/hats/{ID} - Not yet implemented.
+      '': 'hatCollection',        // http://localhost:9000/#            - Not yet implemented.
+      'hats': 'hatCollection',    // http://localhost:9000/#/hats       - Hats module
+      'hats/:id': 'hatCollection' // http://localhost:9000/#/hats/{ID}  - Not yet implemented.
     }
   });
 
@@ -36,7 +37,7 @@ define([
     hatCollection: function() {
       require(['modules/hats/hats'], function (hatsMod) {
         // This fetch logic should move into the module
-        hatsMod.indexHats.fetch().success(function(){ 
+        hatsMod.indexHats.fetch().success(function(){
           // We'll want to load a layout from the module instead
           App.primaryViewport.show(hatsMod.hatCollectionView);
         });
@@ -44,7 +45,8 @@ define([
     }
   });
 
-  // TODO: Add a state model?
+  // TODO: Add a screen/UI state model?
+  // https://devblog.supportbee.com//2011/07/29/backbone-js-tips-lessons-from-the-trenches/
 
   // SET UP START LISTNER =====================================================================
   App.on('start', function() {
